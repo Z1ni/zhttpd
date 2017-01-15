414d9255c5aad4e49bce2a3d43cab250b5665f8d
Zini <marktmakinen@gmail.com>
2017-01-15 03:11:43 +0200

    Restructure main child process loop

--- src/child.c
+++ src/child.c
@@ -5,7 +5,11 @@
 #include "file_io.h"
 #include "cgi.h"
 
-volatile sig_atomic_t run_child_main_loop = 1;
+volatile sig_atomic_t run_child_main_loop = 1;	// True (1) if the main loop should be running
+
+static int sock;					// Socket to use
+static int keep_conn_alive = 0;		// True if the connection is set to be kept alive
+static time_t keepalive_timer = 0;	// Keepalive timer
 
 static void sigint_handler(int signal) {
 	// Parent died or someone wants this process to stop
@@ -31,6 +35,10 @@ static int sendall(int s, char *buf, int len) {
 	}
 
 	return n == -1 ? -1 : total;
+}
+
+static void reset_keepalive_timer(void) {
+	keepalive_timer = time(NULL);
 }
 
 /**
@@ -62,7 +70,206 @@ static int send_error_response(http_request *req, int sock, int status) {
 	return write_res;
 }
 
-void child_main_loop(int sock, pid_t parent_pid) {
+/**
+ * @brief Handle HTTP request
+ * @details Handles given HTTP request and responds to it
+ * 
+ * @param req Request to handle
+ */
+static void handle_http_request(http_request *req) {
+
+	// Check for supported method
+	char *m = req->method;
+	if (strcmp(m, "GET") != 0 && strcmp(m, "POST") != 0 && strcmp(m, "HEAD") != 0) {
+		// Not supported method
+		// Send "501 Not Implemented"
+		send_error_response(req, sock, 501);
+		return;
+	}
+
+	// Concatenate file path and prevent free filesystem access
+	char *final_path;
+	int rp_ret = create_real_path(WEBROOT, strlen(WEBROOT), req->path, strlen(req->path), &final_path);
+	if (rp_ret < 0) {
+		// Invalid path, send "400 Bad Request"
+		send_error_response(req, sock, 400);
+
+	} else {
+		// Valid path
+		zhttpd_log(LOG_INFO, "Client requests file: \"%s\"", final_path);
+
+		// Get file extension
+		char *ext = NULL;
+		char *ext_start = strstr(final_path, ".");
+		if (ext_start != NULL) {
+			ext = (ext_start + sizeof(char));
+			zhttpd_log(LOG_DEBUG, "File extension: %s", ext);
+		}
+
+		if (ext != NULL && strcmp(ext, "php") == 0) {
+			// Run PHP script
+			zhttpd_log(LOG_INFO, "File is runnable PHP file!");
+
+			cgi_parameters params = {
+				.req = req,
+				.script_filename = final_path
+			};
+			unsigned char *php_out;
+			http_header **cgi_headers;
+			size_t cgi_header_count;
+			int cgi_ret = cgi_exec("/usr/bin/php5-cgi", &params, &php_out, &cgi_headers, &cgi_header_count);
+
+			if (cgi_ret < 0 && cgi_ret != ERROR_CGI_STATUS_NONZERO && cgi_ret != ERROR_CGI_SCRIPT_PATH_INVALID) {
+				// Failed
+				zhttpd_log(LOG_ERROR, "PHP execution failed!");
+				// Send "500 Internal Server Error"
+				send_error_response(req, sock, 500);
+
+			} else if (cgi_ret == ERROR_CGI_SCRIPT_PATH_INVALID) {
+				// Script path invalid, send "404 Not Found"
+				send_error_response(req, sock, 404);
+
+			} else if (cgi_ret == ERROR_CGI_STATUS_NONZERO) {
+				// TODO: Handle non-zero status code
+				// For now just send "500 Internal Server Error" instead
+				free(php_out);
+				send_error_response(req, sock, 500);
+
+			} else {
+				// Execution successful, send response
+				// Set headers
+				int flags = CONTENT_SET_CONTENT_TYPE;
+				int status_code = -1;
+
+				for (size_t i = 0; i < cgi_header_count; i++) {
+					http_header *h = cgi_headers[i];
+					char *h_name = string_to_lowercase(h->name);
+					if (strcmp(h_name, "content-type") == 0) {
+						flags = 0;	// Don't guess Content-Type when it's already provided
+					}
+					if (strcmp(h_name, "status") == 0) {
+						// CGI script wants to set the status code
+						// Get status code
+						errno = 0;
+						status_code = strtol(h->value, NULL, 0);
+						if (errno != 0) {
+							zhttpd_log(LOG_ERROR, "CGI status header parsing failed!");
+							status_code = -1;
+						}
+					}
+					free(h_name);
+				}
+
+				http_response *resp = http_response_create((status_code != -1 ? status_code : 200));
+				resp->method = strdup(req->method);
+				resp->keep_alive = keep_conn_alive;
+				resp->fs_path = strdup(final_path);
+				if (strcmp(req->method, "HEAD") == 0) resp->no_payload = 1;	// This is a HEAD response
+				// Add headers to response
+				for (size_t i = 0; i < cgi_header_count; i++) {
+					http_header *h = cgi_headers[i];
+					char *h_name = string_to_lowercase(h->name);
+					if (strcmp(h_name, "status") != 0) {
+						http_response_add_header(resp, h);
+					}
+					free(h_name);
+					http_header_free(h);
+				}
+				free(cgi_headers);
+				// Set content
+				http_response_set_content2(resp, php_out, cgi_ret, flags);
+				free(php_out);
+				char *resp_str;
+				int len = http_response_string(resp, &resp_str);
+				if (len >= 0) {
+					if (sendall(sock, resp_str, len) == -1) {
+						zhttpd_log(LOG_ERROR, "Sendall failed!");
+						perror("sendall");
+					}
+					free(resp_str);
+				}
+				http_response_free(resp);
+			}
+
+		} else {
+
+			unsigned char *file_data;
+			ssize_t file_bytes = read_file(final_path, &file_data);
+			if (file_bytes < 0) {
+				if (file_bytes == ERROR_FILE_IO_NO_ACCESS) {
+					// Respond with "403 Forbidden"
+					send_error_response(req, sock, 403);
+
+				} else if (file_bytes == ERROR_FILE_IO_NO_ENT) {
+					// File not found, respond with "404 File Not Found"
+					send_error_response(req, sock, 404);
+
+				} else if (file_bytes == ERROR_FILE_IO_GENERAL) {
+					// I/O error, response with "500 Internal Server Error"
+					send_error_response(req, sock, 500);
+				}
+				return;
+			}
+
+			// Got file, send response
+
+			http_response *resp = http_response_create(200);
+			resp->method = strdup(req->method);
+			resp->keep_alive = keep_conn_alive;
+			resp->fs_path = strdup(final_path);
+			if (strcmp(req->method, "HEAD") == 0) resp->no_payload = 1;	// This is a HEAD response
+
+			// Check if the request contains If-Modified-Since
+			http_header *if_mod_since_h = http_request_get_header(req, "If-Modified-Since");
+			if (if_mod_since_h != NULL) {
+				// Convert textual time representation to time_t
+				struct tm if_mod_since_tm;
+				if (strptime(if_mod_since_h->value, HTTP_DATE_FORMAT, &if_mod_since_tm) == NULL) {
+					// strptime failed
+					zhttpd_log(LOG_ERROR, "If-Modified-Since date parsing failed!");
+				} else {
+					// strptime succeeded
+					resp->if_mod_since_time = timegm(&if_mod_since_tm);
+				}
+			}
+
+			// Set content
+			// TODO: Do this somewhere else?
+			// TODO: Check case-insensitively
+			if (ext != NULL && strcmp(ext, "css") == 0) {
+				http_response_add_header2(resp, "Content-Type", "text/css");
+				http_response_set_content(resp, file_data, file_bytes);
+			} else {
+				// Guess Content-Type
+				http_response_set_content2(resp, file_data, file_bytes, CONTENT_SET_CONTENT_TYPE);
+			}
+			char *resp_str;
+			int len = http_response_string(resp, &resp_str);
+			if (len >= 0) {
+				if (sendall(sock, resp_str, len) == -1) {
+					zhttpd_log(LOG_ERROR, "Response sending failed!");
+					perror("sendall");
+				}
+				free(resp_str);
+			}
+			http_response_free(resp);
+			free(file_data);
+		}
+
+		free(final_path);
+	}
+}
+
+/**
+ * @brief Child process main loop
+ * @details Reads data from given socket and handles and responds to requests.
+ * 
+ * @param sock Socket to monitor
+ * @param parent_pid Process ID of the parent process
+ */
+void child_main_loop(int in_sock, pid_t parent_pid) {
+
+	sock = in_sock;	// Set global socket variable
 
 	zhttpd_log(LOG_INFO, "Child process started to handle the connection");
 
@@ -102,8 +309,6 @@ void child_main_loop(int sock, pid_t parent_pid) {
 	// Get current time for recv timeout
 	time_t recv_start = time(NULL);
 
-	time_t keepalive_timer = 0;
-
 	// Make given socket nonblocking
 	if (make_socket_nonblocking(sock) == -1) abort();
 
@@ -126,7 +331,6 @@ void child_main_loop(int sock, pid_t parent_pid) {
 
 	int handled = 0;			// False
 	int request_num = 1;		// True
-	int keep_conn_alive = 0;	// False
 	int recv_timer_started = 1;	// True
 
 	// Main event loop
@@ -167,7 +371,7 @@ void child_main_loop(int sock, pid_t parent_pid) {
 
 					buf = calloc(buf_size, sizeof(char));
 					count = read(events[i].data.fd, buf, buf_size);
-					//printf("read: %u\n", count);
+
 					if (count == -1) {
 						// Error
 						if (errno != EAGAIN) {
@@ -212,11 +416,12 @@ void child_main_loop(int sock, pid_t parent_pid) {
 				// Receiving ends
 				// Handle final received data here ================================================
 
-				keepalive_timer = time(NULL);	// TODO: Reset timer somewhere else?
+				reset_keepalive_timer();
 
 				http_request *req;
 				int ret = http_request_parse(received, got_bytes, &req);
 				if (ret < 0) {
+					// Request parsing failed, do something about that
 
 					if (ret == ERROR_PARSER_GET_MORE_DATA) {
 						// Need more data
@@ -224,7 +429,6 @@ void child_main_loop(int sock, pid_t parent_pid) {
 						continue;
 					} else {
 						zhttpd_log(LOG_ERROR, "Request parsing failed with error code %d", ret);
-						//fprintf(stderr, "http_request_parse failed with error code: %d\n", ret);
 						if (ret == ERROR_PARSER_MALFORMED_REQUEST || ret == ERROR_PARSER_NO_HOST_HEADER) {
 							// Malformed request or HTTP/1.1 request without Host header
 							send_error_response(NULL, sock, 400);
@@ -240,7 +444,10 @@ void child_main_loop(int sock, pid_t parent_pid) {
 							send_error_response(NULL, sock, 501);
 						}
 					}
+
 				} else {
+					// Request parsing successful!
+
 					zhttpd_log(LOG_DEBUG, "New HTTP request:");
 					zhttpd_log(LOG_DEBUG, "  Method: %s", req->method);
 					zhttpd_log(LOG_DEBUG, "  Path: %s", req->path);
@@ -260,188 +467,16 @@ void child_main_loop(int sock, pid_t parent_pid) {
 							req->keep_alive = 1;
 						}
 						free(value);
-						if (keep_conn_alive) zhttpd_log(LOG_DEBUG, "Client wants to keep connection alive");
-					}
-
-					if (strcmp(req->method, "GET") == 0 || strcmp(req->method, "POST") == 0 || strcmp(req->method, "HEAD") == 0) {
-						// Concatenate file path and prevent free filesystem access
-						char *final_path;
-						int rp_ret = create_real_path(WEBROOT, strlen(WEBROOT), req->path, strlen(req->path), &final_path);
-						if (rp_ret < 0) {
-							// Invalid path, send "400 Bad Request"
-							send_error_response(req, sock, 400);
-
-						} else {
-							// Valid path
-							zhttpd_log(LOG_INFO, "Client requests file: \"%s\"", final_path);
-
-							// Get file extension
-							char *ext = NULL;
-							char *ext_start = strstr(final_path, ".");
-							if (ext_start != NULL) {
-								ext = (ext_start + sizeof(char));
-								zhttpd_log(LOG_DEBUG, "File extension: %s", ext);
-							}
-
-							if (ext != NULL && strcmp(ext, "php") == 0) {
-								// Run PHP script
-								zhttpd_log(LOG_INFO, "File is runnable PHP file!");
-
-								cgi_parameters params = {
-									.req = req,
-									.script_filename = final_path
-								};
-								unsigned char *php_out;
-								http_header **cgi_headers;
-								size_t cgi_header_count;
-								int cgi_ret = cgi_exec("/usr/bin/php5-cgi", &params, &php_out, &cgi_headers, &cgi_header_count);
-
-								if (cgi_ret < 0 && cgi_ret != ERROR_CGI_STATUS_NONZERO && cgi_ret != ERROR_CGI_SCRIPT_PATH_INVALID) {
-									// Failed
-									zhttpd_log(LOG_ERROR, "PHP execution failed!");
-									// Send "500 Internal Server Error"
-									send_error_response(req, sock, 500);
-
-								} else if (cgi_ret == ERROR_CGI_SCRIPT_PATH_INVALID) {
-									// Script path invalid, send "404 Not Found"
-									send_error_response(req, sock, 404);
-
-								} else if (cgi_ret == ERROR_CGI_STATUS_NONZERO) {
-									// TODO: Handle non-zero status code
-									// For now just send "500 Internal Server Error" instead
-									free(php_out);
-									send_error_response(req, sock, 500);
-
-								} else {
-									// Execution successful, send response
-									// Set headers
-									int flags = CONTENT_SET_CONTENT_TYPE;
-									int status_code = -1;
-
-									for (size_t i = 0; i < cgi_header_count; i++) {
-										http_header *h = cgi_headers[i];
-										char *h_name = string_to_lowercase(h->name);
-										if (strcmp(h_name, "content-type") == 0) {
-											flags = 0;	// Don't guess Content-Type when it's already provided
-										}
-										if (strcmp(h_name, "status") == 0) {
-											// CGI script wants to set the status code
-											// Get status code
-											errno = 0;
-											status_code = strtol(h->value, NULL, 0);
-											if (errno != 0) {
-												zhttpd_log(LOG_ERROR, "CGI status header parsing failed!");
-												status_code = -1;
-											}
-										}
-										free(h_name);
-									}
-
-									http_response *resp = http_response_create((status_code != -1 ? status_code : 200));
-									resp->method = strdup(req->method);
-									resp->keep_alive = keep_conn_alive;
-									resp->fs_path = strdup(final_path);
-									if (strcmp(req->method, "HEAD") == 0) resp->no_payload = 1;	// This is a HEAD response
-									// Add headers to response
-									for (size_t i = 0; i < cgi_header_count; i++) {
-										http_header *h = cgi_headers[i];
-										char *h_name = string_to_lowercase(h->name);
-										if (strcmp(h_name, "status") != 0) {
-											http_response_add_header(resp, h);
-										}
-										free(h_name);
-										http_header_free(h);
-									}
-									free(cgi_headers);
-									// Set content
-									http_response_set_content2(resp, php_out, cgi_ret, flags);
-									free(php_out);
-									char *resp_str;
-									int len = http_response_string(resp, &resp_str);
-									if (len >= 0) {
-										if (sendall(sock, resp_str, len) == -1) {
-											zhttpd_log(LOG_ERROR, "Sendall failed!");
-											perror("sendall");
-										}
-										free(resp_str);
-									}
-									http_response_free(resp);
-								}
-
-							} else {
-
-								unsigned char *file_data;
-								ssize_t file_bytes = read_file(final_path, &file_data);
-								if (file_bytes < 0) {
-									if (file_bytes == ERROR_FILE_IO_NO_ACCESS) {
-										// Respond with "403 Forbidden"
-										send_error_response(req, sock, 403);
-
-									} else if (file_bytes == ERROR_FILE_IO_NO_ENT) {
-										// File not found, respond with "404 File Not Found"
-										send_error_response(req, sock, 404);
-
-									} else if (file_bytes == ERROR_FILE_IO_GENERAL) {
-										// I/O error, response with "500 Internal Server Error"
-										send_error_response(req, sock, 500);
-									}
-								} else {
-									// Got file, send response
-
-									http_response *resp = http_response_create(200);
-									resp->method = strdup(req->method);
-									resp->keep_alive = keep_conn_alive;
-									resp->fs_path = strdup(final_path);
-									if (strcmp(req->method, "HEAD") == 0) resp->no_payload = 1;	// This is a HEAD response
-
-									// Check if the request contains If-Modified-Since
-									http_header *if_mod_since_h = http_request_get_header(req, "If-Modified-Since");
-									if (if_mod_since_h != NULL) {
-										// Convert textual time representation to time_t
-										struct tm if_mod_since_tm;
-										if (strptime(if_mod_since_h->value, HTTP_DATE_FORMAT, &if_mod_since_tm) == NULL) {
-											// strptime failed
-											zhttpd_log(LOG_ERROR, "If-Modified-Since date parsing failed!");
-										} else {
-											// strptime succeeded
-											resp->if_mod_since_time = timegm(&if_mod_since_tm);
-										}
-									}
-
-									// Set content
-									// TODO: Do this somewhere else?
-									// TODO: Check case-insensitively
-									if (ext != NULL && strcmp(ext, "css") == 0) {
-										http_response_add_header2(resp, "Content-Type", "text/css");
-										http_response_set_content(resp, file_data, file_bytes);
-									} else {
-										// Guess Content-Type
-										http_response_set_content2(resp, file_data, file_bytes, CONTENT_SET_CONTENT_TYPE);
-									}
-									char *resp_str;
-									int len = http_response_string(resp, &resp_str);
-									if (len >= 0) {
-										if (sendall(sock, resp_str, len) == -1) {
-											zhttpd_log(LOG_ERROR, "Response sending failed!");
-											perror("sendall");
-										}
-										free(resp_str);
-									}
-									http_response_free(resp);
-									free(file_data);
-									keepalive_timer = time(NULL);	// TODO: Reset timer somewhere else?
-								}
-							}
-
-							free(final_path);
+						if (keep_conn_alive) {
+							zhttpd_log(LOG_DEBUG, "Client wants to keep connection alive");
+							reset_keepalive_timer();
 						}
-
-					} else {
-						// Not supported method
-						// Send "501 Not Implemented"
-						send_error_response(req, sock, 501);
 					}
 
+					// Handle the request and respond to it
+					handle_http_request(req);
+
+					// We're done with the request, free it
 					http_request_free(req);
 				}
 
@@ -454,7 +489,7 @@ void child_main_loop(int sock, pid_t parent_pid) {
 				handled = 1;
 				if (keep_conn_alive) {
 					zhttpd_log(LOG_DEBUG, "Starting keepalive timer");
-					keepalive_timer = time(NULL);
+					reset_keepalive_timer();
 					request_num++;
 					recv_buf_size = buf_size;
 					received = calloc(recv_buf_size, sizeof(char));
@@ -477,6 +512,9 @@ void child_main_loop(int sock, pid_t parent_pid) {
 		if (keep_conn_alive && time(NULL) - keepalive_timer >= REQUEST_KEEPALIVE_TIMEOUT_SECONDS) {
 			// Keep-alive timeout
 			// Just close connection for now
+			/* TODO: Have some kind of "processing" variable that indicates if
+			 *       data processing is in progress and reset the timer in that case.
+			 */
 			zhttpd_log(LOG_INFO, "Client connection keep-alive timeout");
 
 			run_child_main_loop = 0;
